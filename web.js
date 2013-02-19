c305a7b000d25c048ef1a6992b8278ddb6051533
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-02-18 16:37:12 -0800

    replace logging with connect logger

--- web.js
+++ web.js
@@ -2,47 +2,47 @@ var fs = require('fs');
 var path = require('path');
 var port = process.env.PORT || 8081; 
 var connect = require('connect');
-connect.createServer(function (request, response) {
- 
-    console.log(request.url)
-     
-    var filePath = '.' + request.url;
-    if (filePath.match('/[^.]*$')) filePath += '/'
-    if (filePath.match('/$')) filePath += 'index.html'
+connect.createServer(
+    connect.logger('dev'),
+    function (request, response) { 
          
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.jpeg':
-            contentType = 'image/jpeg';
-            break;
-    }
-     
-    fs.exists(filePath, function(exists) {
-     
-        if (exists) {
-            fs.readFile(filePath, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                }
-                else {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                }
-            });
+        var filePath = '.' + request.url;
+        if (filePath.match('/[^.]*$')) filePath += '/'
+        if (filePath.match('/$')) filePath += 'index.html'
+             
+        var extname = path.extname(filePath);
+        var contentType = 'text/html';
+        switch (extname) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.jpeg':
+                contentType = 'image/jpeg';
+                break;
         }
-        else {
-            response.writeHead(404);
-            response.end();
-        }
-    });
+         
+        fs.exists(filePath, function(exists) {
+         
+            if (exists) {
+                fs.readFile(filePath, function(error, content) {
+                    if (error) {
+                        response.writeHead(500);
+                        response.end();
+                    }
+                    else {
+                        response.writeHead(200, { 'Content-Type': contentType });
+                        response.end(content, 'utf-8');
+                    }
+                });
+            }
+            else {
+                response.writeHead(404);
+                response.end();
+            }
+        });
      
 }).listen(port);
  
