ba52eaf35867ac3c115693e280914aed42d9f647
Odysseas Tsatalos <odysseas@tsatalos.com>
2013-02-18 20:22:09 -0800

    use static + favicon middlewares

--- web.js
+++ web.js
@@ -3,47 +3,9 @@ var path = require('path');
 var port = process.env.PORT || 8081; 
 var connect = require('connect');
 connect.createServer(
-    connect.logger(),
-    function (request, response) { 
-         
-        var filePath = '.' + request.url;
-        if (filePath.match('/[^.]*$')) filePath += '/'
-        if (filePath.match('/$')) filePath += 'index.html'
-             
-        var extname = path.extname(filePath);
-        var contentType = 'text/html';
-        switch (extname) {
-            case '.js':
-                contentType = 'text/javascript';
-                break;
-            case '.css':
-                contentType = 'text/css';
-                break;
-            case '.jpeg':
-                contentType = 'image/jpeg';
-                break;
-        }
-         
-        fs.exists(filePath, function(exists) {
-         
-            if (exists) {
-                fs.readFile(filePath, function(error, content) {
-                    if (error) {
-                        response.writeHead(500);
-                        response.end();
-                    }
-                    else {
-                        response.writeHead(200, { 'Content-Type': contentType });
-                        response.end(content, 'utf-8');
-                    }
-                });
-            }
-            else {
-                response.writeHead(404);
-                response.end();
-            }
-        });
-     
-}).listen(port);
+    connect.logger('dev'),
+    connect.favicon('public/favicon.ico'),
+    connect.static(__dirname + '/public')
+).listen(port);
  
 console.log('Server running at http://127.0.0.1:'+port+'/');
